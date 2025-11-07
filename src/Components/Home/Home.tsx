f46a37f36e6bf53a404e8db21c56404024c07c50
100sorting <bjl5029@gmail.com>
2025-11-07 10:38:05 +0900

    모바일에서 minit icon size 축소

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -85,7 +85,6 @@ const Home: React.FC = () => {
             // 아이콘 span 생성
             const span = document.createElement('span');
             span.setAttribute('data-icon-pixel-size', isMobile ? '20' : '80');
-            span.setAttribute('data-icon-pixel-size', '80');
             span.setAttribute('data-chat-small-medium-or-large', 'small');
             span.setAttribute('data-circle-or-square', 'circle');
             span.setAttribute('data-left-or-right', 'right');
